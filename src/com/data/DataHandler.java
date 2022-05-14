e3b291d374e51b72920471cc373d0d876a182ba6
thuy <s3877746@rmit.edu.vn>
2022-05-15 02:03:32 +0700

    final version

--- src/com/data/DataHandler.java
+++ src/com/data/DataHandler.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class DataHandler {
     public static void readFile(MyArrayList<Customer> s) throws Exception {
-        BufferedReader reader = new BufferedReader(new FileReader("D:\\IntellJ\\IdeaProjects\\dsa\\src\\com\\data\\customer.csv")); // read file
+        BufferedReader reader = new BufferedReader(new FileReader("D:\\IntellJ\\IdeaProjects\\Project_Group5\\dsa\\src\\com\\data\\customer.csv")); // read file
         reader.readLine(); // skip 1st line
         String row;
 
